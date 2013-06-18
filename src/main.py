75375ffb8b6b7132633d2cec436f93eaafc11004
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2013-06-17 22:08:04 -0400

    header in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+# Quasar Correlated Lyman alpha forest mocks
+#
+#  Mosaic (Nested Mesh) Mocks
+#
+#  Yu Feng 2013, Carnegie Mellon U
+# 
+#  input: a paramfile
+#  run:
+#      sightlines (if geometry in paramfile is not test)
+#                 (less expensive, with as many cores as possible)
+#      gaussian  (expensive, with as many cores as possible)
+#      lognormal (cheap, with as few cores as possible)
+#      matchmeanflux (cheap, don't run with too many cores)
+#      makespectra (cheap, don't run with too many cores)
+#
+#  typical mock takes 2 hour on 16 core good machines, 
+#              <30 min on 64 core
+#  make sure fine mesh don't use more than memory per core.
+#
 import numpy
 import commands as cmd
 from args import parseargs
